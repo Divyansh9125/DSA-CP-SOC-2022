072bca3869fea08b97d1b37ac14c5b55956fad10
Spandan Anaokar <spandananao@gmail.com>
2022-05-11 14:32:37 +0530

    Update Two Sets.py

--- Introductory Problems/Two Sets.py
+++ Introductory Problems/Two Sets.py
@@ -1,11 +1,31 @@
-def comp(arr, n, t):
-    
-
-
 n = int(input())
 if n*(n+1)%4!=0:
     print("NO")
+elif n%4==0:
+    print("YES")
+    print(n//2)
+    for i in range(1,n+1,4):
+        print(i, end = " ")
+    for i in range(4,n+1,4):
+        print(i, end = " ")
+    print()
+    print(n//2)
+    for i in range(2,n+1,4):
+        print(i, end = " ")
+    for i in range(3,n+1,4):
+        print(i, end = " ")
 else:
-    t = n*(n+1)//4
-    arr = []
-    arr = comp(arr, n, t)
+    print("YES")
+    print((n-3)//2+2)
+    print("1 2", end = " ")
+    for i in range(n,3,-4):
+        print(i, end = " ")
+    for i in range(n-3,3,-4):
+        print(i, end = " ")
+    print()
+    print((n-3)//2+1)
+    print("3", end = " ")
+    for i in range(n-1,3,-4):
+        print(i, end = " ")
+    for i in range(n-2, 3,-4):
+        print(i, end = " ")
